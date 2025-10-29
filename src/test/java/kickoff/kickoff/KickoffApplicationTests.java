7319ef97b57bfc816e6113afd362c43f84112e18
Visnu Visalini <visalinivisnu@gmail.com>
2025-10-29 10:40:51 +0530

    Initial commit

---
+++ src/test/java/kickoff/kickoff/KickoffApplicationTests.java
@@ -0,0 +1,13 @@
+package kickoff.kickoff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickoffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
